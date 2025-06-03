c96585d15d5e0eee463526add5493f8db0db47b3
DSLJDI <54802522+DSLJDI@users.noreply.github.com>
2025-06-03 13:36:30 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
 torchrun \
     --standalone \
     --nnodes=1 \
-    --nproc_per_node=4 \
+    --nproc_per_node=1 \
 main_train.py \
     --world_size 1 \
     --batch_size 16 \
-    --data_path "/CASIA2.0" \
+    --data_path "The location of your combined dataset configuration file" \
     --epochs 200 \
     --lr 2e-4 \
     --min_lr 5e-7 \
@@ -13,9 +13,9 @@ main_train.py \
     --pretrain_path "/uniformer_base_ls_in1k.pth" \
     --test_data_path "/CASIA1.0" \
     --warmup_epochs 4 \
-    --output_dir ./output_dir_window_overall/ \
-    --log_dir ./output_dir_window_overall/  \
-    --accum_iter 16 \
+    --output_dir ./output_dir/ \
+    --log_dir ./output_dir/  \
+    --accum_iter 1 \
     --seed 42 \
     --test_period 4 \
     --num_workers 8 \
